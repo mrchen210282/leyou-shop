9daa0f1ebd3a5e54bd67df3aff3855b52b43e3c1
wj <wj@bitflash.vip>
2019-01-24 11:11:08 +0800

    提交

--- leyou-order/src/main/java/com/leyou/order/service/OrderService.java
+++ leyou-order/src/main/java/com/leyou/order/service/OrderService.java
@@ -6,11 +6,10 @@ import com.leyou.auth.entiy.UserInfo;
 import com.leyou.common.PageResult;
 import com.leyou.order.interceptor.LoginInterceptor;
 import com.leyou.order.mapper.OrderDetailMapper;
+import com.leyou.order.mapper.OrderInformationMapper;
 import com.leyou.order.mapper.OrderMapper;
 import com.leyou.order.mapper.OrderStatusMapper;
-import com.leyou.order.pojo.Order;
-import com.leyou.order.pojo.OrderDetail;
-import com.leyou.order.pojo.OrderStatus;
+import com.leyou.order.pojo.*;
 import com.leyou.utils.IdWorker;
 import com.leyou.utils.PayHelper;
 import org.slf4j.Logger;
@@ -19,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -40,6 +41,9 @@ public class OrderService {
 
     @Autowired
     private OrderStatusMapper statusMapper;
+
+    @Autowired
+    private OrderInformationMapper orderInformationMapper;
 
     private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
@@ -135,6 +139,77 @@ public class OrderService {
         }
         int count = this.statusMapper.updateByPrimaryKeySelective(record);
         return count == 1;
+    }
+
+
+    /**
+     * 查询我的中的订单信息
+     *
+     * @param status 1、未付款 2、已付款,未发货 3、已发货,未确认 4、交易成功 5、交易关闭 6、已评价
+     * @return
+     */
+    public List<MyOrderDetail> queryOrder(String status) {
+        List<MyOrderDetail> myOrderList = new ArrayList<MyOrderDetail>();
+        // 获取登录用户
+        UserInfo user = LoginInterceptor.getLoginUser();
+
+        List<MyOrderDetail> myOrderDetailList = new ArrayList<MyOrderDetail>();
+
+        List<OrderInformation> list = this.orderInformationMapper.queryOrder(status, user.getId());
+        if (null != list && list.size() > 0) {
+            for(int i = 0; i < list.size(); i++) {
+                OrderInformation orderDetail = list.get(i);
+                for(int j = 0; j < list.size(); j++) {
+                    OrderInformation order = list.get(j);
+                    if(orderDetail.getOrderId().equals(order.getOrderId())) {
+                        BigDecimal bigDecimal = new BigDecimal(0);
+                        OrderInformation orderDetail1 = list.get(j);
+                        MyOrderDetail myOrderDetail = new MyOrderDetail();
+
+                        myOrderDetail.setOrderId(orderDetail1.getOrderId());
+                        myOrderDetail.setSkuId(orderDetail1.getSkuId());
+                        String orderStatus = orderDetail1.getStatus();
+                        if("1".equals(orderStatus)) {
+                            myOrderDetail.setDesc("未付款");
+                            myOrderDetail.setStatus("1");
+                        } else if ("2".equals(orderStatus)) {
+                            myOrderDetail.setDesc("待收货");
+                            myOrderDetail.setStatus("2");
+                        } else if ("3".equals(orderStatus)) {
+                            myOrderDetail.setDesc("待收货");
+                            myOrderDetail.setStatus("3");
+                        } else if ("4".equals(orderStatus)) {
+                            myOrderDetail.setDesc("已完成");
+                            myOrderDetail.setStatus("4");
+                        } else if ("5".equals(orderStatus)) {
+                            myOrderDetail.setDesc("交易关闭");
+                            myOrderDetail.setStatus("5");
+                        } else {
+                            myOrderDetail.setDesc("已完成");
+                            myOrderDetail.setStatus("6");
+                        }
+                        //算
+                        bigDecimal.add(orderDetail1.getPrice());
+
+                        List<OrderMessage> orderMessagesList = new ArrayList<OrderMessage>();
+
+                        OrderMessage orderMessag = new OrderMessage();
+                        orderMessag.setImage(orderDetail1.getImageURL());
+                        orderMessag.setNum(orderDetail1.getQuantity());
+                        orderMessag.setTitle(orderDetail1.getTitle());
+                        orderMessag.setPrice(orderDetail1.getPrice());
+                        orderMessagesList.add(orderMessag);
+
+                        myOrderDetail.setList(orderMessagesList);
+
+                        myOrderDetailList.add(myOrderDetail);
+
+                    }
+                }
+            }
+        }
+        return myOrderDetailList;
+
     }
 
 }
